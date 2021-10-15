92e430a653dd22638343c43ec4b399f4d556f387
Freddy Jilson <jilsonfreddy@hotmail.com>
2021-10-15 18:45:58 +0800

    1 Add proj support for CLI extensibility

--- Makefile
+++ Makefile
@@ -1,20 +1,14 @@
 #!make
 
-all:
-	# Install all networking tools
-
-install-example-cli:
-	go build -ldflags '-w -s' -a -o ./bin/example_cli ./src/example_cli/main.go; \
-    chmod +x ./bin/example_cli; \
-
-install-subnet-calculator:
-	go build -ldflags '-w -s' -a -o ./bin/subnet_calculator ./src/subnet_calculator/main.go; \
-    chmod +x ./bin/subnet_calculator; \
+all: ci-lint
+	# Install netools
+	go build -ldflags '-w -s' -a -o ./bin/netools ./src/cmd/cmd.go; \
+    chmod +x ./bin/netools; \
+    echo 'netools is installed in the bin directory'
 
 ci-lint:
 	chmod +x ./bin/golangci-lint; \
 	./bin/golangci-lint run; \
 
 clean:
-	rm -rf ./bin/example_cli; \
-	rm -rf ./bin/subnet_calculator; \
+	rm -rf ./bin/netools;
---
+++ src/cli/cli.go
@@ -0,0 +1,37 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "netools",
+	Short: "A brief description of your application",
+	Long:  `Welcome to netools`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.rootcli.yaml)")
+
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {}
