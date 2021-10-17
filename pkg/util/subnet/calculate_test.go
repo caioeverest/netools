68226d15181d74a01670ad72d4b1c97171b8481c
NikhilMJagtap <nikhil.jagtap@workindia.in>
2021-10-17 18:30:56 +0530

    changed to table tests

--- pkg/util/subnet/calculate_test.go
+++ pkg/util/subnet/calculate_test.go
@@ -8,105 +8,44 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func testAssertionHelper(
-	t *testing.T,
-	trueValues []interface{},
-	networkAddress string,
-	broadcastAddress string,
-	noOfHosts int,
-	wildcardMask string,
-	cidrNotation int,
-) {
-	assert.Equal(t, trueValues[0], networkAddress)
-	assert.Equal(t, trueValues[1], broadcastAddress)
-	assert.Equal(t, trueValues[2], noOfHosts)
-	assert.Equal(t, trueValues[3], wildcardMask)
-	assert.Equal(t, trueValues[4], cidrNotation)
-}
-
-func TestSubnetCalculateClassC(t *testing.T) {
-	// testing subnet for class C
-	ip := "198.168.1.101"
-	mask := "255.255.255.128"
-
-	trueValues := []interface{}{
-		"198.168.1.0", "198.168.1.127", 126, "0.0.0.127", 25,
+func TestCalculate(t *testing.T) {
+	var tests = []struct {
+		IPAddress                string
+		SubnetMask               string
+		ExpectedNetworkAddress   string
+		ExpectedBroadcastAddress string
+		ExpectedNoOfHosts        int
+		ExpectedWildcardMask     string
+		ExpectedCidrNotation     int
+	}{
+		{"198.168.1.101", "255.255.255.128", "198.168.1.0", "198.168.1.127", 126, "0.0.0.127", 25},
+		{"198.168.1.101", "255.255.192.0", "198.168.0.0", "198.168.63.255", 16382, "0.0.63.255", 18},
+		{"198.168.1.101", "255.248.0.0", "198.168.0.0", "198.175.255.255", 524286, "0.7.255.255", 13},
+		{"52.12.12.52", "128.0.0.0", "0.0.0.0", "127.255.255.255", 2147483646, "127.255.255.255", 1},
+		{"52.12.12.52", "128.0.0.0", "0.0.0.0", "127.255.255.255", 2147483646, "127.255.255.255", 1},
 	}
-
-	networkAddress, broadcastAddress, noOfHosts, wildcardMask, cidrNotation := Calculate(ip, mask)
-	testAssertionHelper(t, trueValues, networkAddress, broadcastAddress, noOfHosts, wildcardMask, cidrNotation)
-}
-
-func TestSubnetCalculateClassB(t *testing.T) {
-	// testing subnet for class B
-	ip := "198.168.1.101"
-	mask := "255.255.192.0"
-
-	trueValues := []interface{}{
-		"198.168.0.0", "198.168.63.255", 16382, "0.0.63.255", 18,
+	for _, test := range tests {
+		actualNetworkAddress, actualBroadcastAddress, actualNoOfHosts, actualWildCardMask, actualCidrNotation := Calculate(test.IPAddress, test.SubnetMask)
+		assert.Equal(t, test.ExpectedNetworkAddress, actualNetworkAddress)
+		assert.Equal(t, test.ExpectedBroadcastAddress, actualBroadcastAddress)
+		assert.Equal(t, test.ExpectedNoOfHosts, actualNoOfHosts)
+		assert.Equal(t, test.ExpectedWildcardMask, actualWildCardMask)
+		assert.Equal(t, test.ExpectedCidrNotation, actualCidrNotation)
 	}
-
-	networkAddress, broadcastAddress, noOfHosts, wildcardMask, cidrNotation := Calculate(ip, mask)
-	testAssertionHelper(t, trueValues, networkAddress, broadcastAddress, noOfHosts, wildcardMask, cidrNotation)
 }
 
-func TestSubnetCalculateClassA(t *testing.T) {
-	// testing subnet for class A
-	ip := "198.168.1.101"
-	mask := "255.248.0.0"
-
-	trueValues := []interface{}{
-		"198.168.0.0", "198.175.255.255", 524286, "0.7.255.255", 13,
+func TestIsValidSubnetMask(t *testing.T) {
+	var tests = []struct {
+		SubnetMask      string
+		ExpectedIsValid bool
+	}{
+		{"255.255.255.128", true},
+		{"255.255.128.0", true},
+		{"255.128.255.0", false},
+		{"0.0.128.0", false},
 	}
-
-	networkAddress, broadcastAddress, noOfHosts, wildcardMask, cidrNotation := Calculate(ip, mask)
-	testAssertionHelper(t, trueValues, networkAddress, broadcastAddress, noOfHosts, wildcardMask, cidrNotation)
-}
-
-func TestSubnetCalculateTest1(t *testing.T) {
-	ip := "52.12.12.52"
-	mask := "128.0.0.0"
-
-	trueValues := []interface{}{
-		"0.0.0.0", "127.255.255.255", 2147483646, "127.255.255.255", 1,
+	for _, test := range tests {
+		maskInt := binary.BigEndian.Uint32(net.ParseIP(test.SubnetMask)[12:16])
+		assert.Equal(t, test.ExpectedIsValid, IsValidSubnetMask(maskInt))
 	}
-
-	networkAddress, broadcastAddress, noOfHosts, wildcardMask, cidrNotation := Calculate(ip, mask)
-	testAssertionHelper(t, trueValues, networkAddress, broadcastAddress, noOfHosts, wildcardMask, cidrNotation)
-}
-
-func TestSubnetCalculateTest2(t *testing.T) {
-	ip := "52.12.12.52"
-	mask := "128.0.0.0"
-
-	trueValues := []interface{}{
-		"0.0.0.0", "127.255.255.255", 2147483646, "127.255.255.255", 1,
-	}
-
-	networkAddress, broadcastAddress, noOfHosts, wildcardMask, cidrNotation := Calculate(ip, mask)
-	testAssertionHelper(t, trueValues, networkAddress, broadcastAddress, noOfHosts, wildcardMask, cidrNotation)
-}
-
-func TestIsValidSubnetMask1(t *testing.T) {
-	subnetMask := "255.255.255.128"
-	maskInt := binary.BigEndian.Uint32(net.ParseIP(subnetMask)[12:16])
-	assert.Equal(t, true, IsValidSubnetMask(maskInt))
-}
-
-func TestIsValidSubnetMask2(t *testing.T) {
-	subnetMask := "255.255.128.0"
-	maskInt := binary.BigEndian.Uint32(net.ParseIP(subnetMask)[12:16])
-	assert.Equal(t, true, IsValidSubnetMask(maskInt))
-}
-
-func TestIsValidSubnetMask3(t *testing.T) {
-	subnetMask := "255.128.255.0"
-	maskInt := binary.BigEndian.Uint32(net.ParseIP(subnetMask)[12:16])
-	assert.Equal(t, false, IsValidSubnetMask(maskInt))
-}
-
-func TestIsValidSubnetMask4(t *testing.T) {
-	subnetMask := "0.0.128.0"
-	maskInt := binary.BigEndian.Uint32(net.ParseIP(subnetMask)[12:16])
-	assert.Equal(t, false, IsValidSubnetMask(maskInt))
 }
